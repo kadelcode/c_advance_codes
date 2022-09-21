df3dc646188ccfed92082605762498cab4918dad
kadelcode <kadelcode@gmail.com>
2022-09-21 23:51:43 +0100

    Student details program

--- File Handling/file_ops.c
+++ File Handling/file_ops.c
@@ -2,11 +2,32 @@
 
 
 void main() {
+    char name[50];
+    int i, marks, num;
+
+    printf("Enter the number of students:");
+    scanf("%d", num);
     // Declaring a file
     FILE *fp;
-    
+
     // Opening a file
     fp = fopen("Student_Details.txt", "w");
+
+    if(fp == NULL) {
+        printf("Error!");
+    }
+
+    for (i=0; i<num; i++) {
+        printf("Enter name for student %d\n",i);
+        scanf("%c", name);
+        printf("Enter mark for student %d",i);
+        scanf("%d",marks);
+
+        fprintf(fp,"\nName: %s \nMarks=%d \n", name, marks);
+    }
+       
+    
+    
 
     fclose(fp);
     
